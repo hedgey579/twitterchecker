03e75029690b45942927986eebdfa64594f21cc9
Alexander Berry <berryalex123@gmail.com>
2017-05-10 14:00:27 -0700

    Added functional ParseDictionary class and self containing files

--- src/config/ParseDictionary.java
+++ src/config/ParseDictionary.java
@@ -1,45 +1,22 @@
 package config;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ParseDictionary {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        List<String> words = new ArrayList<String>(); 
-        BufferedReader inFile = null;
-		try {
-			inFile = new BufferedReader (new FileReader("words.txt"));
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-        String inputLine;
-        try {
-			while ((inputLine = inFile.readLine())!=null){ 
-			    try {
-					words.add(inFile.readLine());
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+		public static void main(String[] args){
+			Scanner s = null;
+			try {
+				s = new Scanner(new File("src/files/words.txt"));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        System.out.print(words.get(3));
-        try {
-			inFile.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			List<String> words = new ArrayList<String>();
+			while(s.hasNext())
+					words.add(s.next());
+			s.close();
 	}
-
 }
